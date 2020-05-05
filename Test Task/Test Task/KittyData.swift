4c172e8f567530d2c8f599be87f41fa0b60a8ec5
Anton <pustovidko.anton@icloud.com>
2020-05-05 14:25:30 +0300

    a structure for cats data

---
+++ Test Task/Test Task/KittyData.swift
@@ -0,0 +1,17 @@
+//
+//  KittyData.swift
+//  Test Task
+//
+//  Created by Anton on 5/5/20.
+//  Copyright © 2020 falli_ot. All rights reserved.
+//
+
+import Foundation
+struct KittyData : Codable {
+
+let id: String
+let name: String
+let temperament: String
+let origin: String
+
+}
