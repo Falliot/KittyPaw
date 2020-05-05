810f1260541a96676da1d44816b84d324943ab71
Anton <pustovidko.anton@icloud.com>
2020-05-05 14:24:01 +0300

    custom table view cell

---
+++ Test Task/Test Task/KittyTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  KittyTableViewCell.swift
+//  Test Task
+//
+//  Created by Anton on 5/5/20.
+//  Copyright © 2020 falli_ot. All rights reserved.
+//
+
+import UIKit
+
+class KittyTableViewCell: UITableViewCell {
+
+  @IBOutlet weak var cellView: UIView!
+  
+  @IBOutlet weak var imgView: UIImageView!
+  
+  @IBOutlet weak var nameLbl: UILabel!
+  
+  @IBOutlet weak var originLbl: UILabel!
+  
+  override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
