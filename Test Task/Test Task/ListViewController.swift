77730b6211be487ae5db4cc6d2398cd027c12ad1
Anton <pustovidko.anton@icloud.com>
2020-05-05 15:56:46 +0300

    add search bar

--- Test Task/Test Task/ListViewController.swift
+++ Test Task/Test Task/ListViewController.swift
@@ -9,62 +9,75 @@
 import UIKit
 
 class ListViewController: UIViewController {
-
-  @IBOutlet weak var tableView: UITableView!
   
-  var kittyData = [KittyData]()
+  @IBOutlet weak var tableView: UITableView!
+  @IBOutlet weak var searchBar: UISearchBar!
+  
+  var kittyArray = [KittyData]()
+  
+  var filteredKitties = [KittyData]()
+  
+  let searchController = UISearchController(searchResultsController: nil)
+  
   
   override func viewDidLoad() {
-        super.viewDidLoad()
+    super.viewDidLoad()
     
     getKitties()
     configureDelegates()
-    
-        // Do any additional setup after loading the view.
-    }
- 
+    alterLayout()
+    // Do any additional setup after loading the view.
+  }
+  
   func configureDelegates() {
+    searchBar.delegate = self
     tableView.delegate = self
     tableView.dataSource = self
   }
   
+  func alterLayout() {
+    navigationController?.hidesBarsOnSwipe = false
+    searchBar.showsScopeBar = false // you can show/hide this dependant on your layout
+    searchBar.placeholder = "Search Kitty by Origin"
+  }
   
-   func getKitties() {
-     guard let resourceURL = URL(string:"https://api.thecatapi.com/v1/breeds") else {fatalError()}
-     var urlRequest = URLRequest(url: resourceURL)
-     urlRequest.httpMethod = "GET"
-     urlRequest.setValue("eb4517d5-865b-4e49-9b2f-96acfa53c0b2", forHTTPHeaderField: "x-api-key")
-     
-     URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-       
-       if error != nil {
-         print("Error occured: \(String(describing: error))")
-         return
-       }
-       
-       guard let safeData = data  else { return }
-       do {
-         
-         let decodingData = try JSONDecoder().decode([KittyData].self, from: safeData)
-        self.kittyData = decodingData
+  
+  func getKitties() {
+    guard let resourceURL = URL(string:"https://api.thecatapi.com/v1/breeds") else {fatalError()}
+    var urlRequest = URLRequest(url: resourceURL)
+    urlRequest.httpMethod = "GET"
+    urlRequest.setValue("eb4517d5-865b-4e49-9b2f-96acfa53c0b2", forHTTPHeaderField: "x-api-key")
+    
+    URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+      
+      if error != nil {
+        print("Error occured: \(String(describing: error))")
+        return
+      }
+      
+      guard let safeData = data  else { return }
+      do {
+        
+        let decodingData = try JSONDecoder().decode([KittyData].self, from: safeData)
+        self.kittyArray = decodingData
+        self.filteredKitties = self.kittyArray
         DispatchQueue.main.async {
           self.tableView.reloadData()
         }
-         print("Kitties list: \(decodingData)")
-         
-       } catch {
-         print("Decoder error:  \(String(describing: error))")
-       }
-     }.resume()
-   }
-  
+        print("Kitties list: \(decodingData)")
+        
+      } catch {
+        print("Decoder error:  \(String(describing: error))")
+      }
+    }.resume()
+  }
   
   
 }
 
 extension ListViewController: UITableViewDelegate, UITableViewDataSource {
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    kittyData.count
+    kittyArray.count
   }
   
   func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -76,7 +89,7 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     
     cell.cellView.layer.cornerRadius = cell.cellView.frame.height / 2
     
-    let kittyCell = kittyData[indexPath.row]
+    let kittyCell = kittyArray[indexPath.row]
     
     cell.nameLbl.text? = kittyCell.name
     cell.originLbl.text? = kittyCell.origin
@@ -85,6 +98,21 @@ extension ListViewController: UITableViewDelegate, UITableViewDataSource {
     cell.imgView.layer.cornerRadius = cell.imgView.frame.height / 2
     
     return cell
+  }
+}
+
+extension ListViewController: UISearchBarDelegate {
+ 
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    guard !searchText.isEmpty else {
+      kittyArray = filteredKitties
+      tableView.reloadData()
+      return
+    }
+    kittyArray = filteredKitties.filter({ kitty -> Bool in
+      kitty.origin.lowercased().contains(searchText.lowercased())
+    })
+    tableView.reloadData()
   }
   
   
