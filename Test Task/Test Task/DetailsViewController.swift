6f0cb45ed105269ac29d0932f5fc6ad7f1cbc0e9
Anton <pustovidko.anton@icloud.com>
2020-05-06 12:11:19 +0300

    display an image of the kitty

--- Test Task/Test Task/DetailsViewController.swift
+++ Test Task/Test Task/DetailsViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class DetailsViewController: UIViewController {
-
   
-  var kittyDetails: KittyData?
+  
+  var kittyDetails: BreedImg?
   
   @IBOutlet weak var imgView: UIImageView!
   
+  var imageURL = String()
   
   @IBOutlet weak var nameLbl: UILabel!
   
@@ -25,24 +26,41 @@ class DetailsViewController: UIViewController {
   @IBOutlet weak var temperamentLbl: UILabel!
   
   override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-      
-    nameLbl.text = kittyDetails?.name
-    originLbl.text = kittyDetails?.origin
-    idLbl.text = kittyDetails?.id
-    temperamentLbl.text = kittyDetails?.temperament
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+    
+    nameLbl.text = kittyDetails?.breeds.first?.name
+    originLbl.text = kittyDetails?.breeds.first?.origin
+    idLbl.text = kittyDetails?.breeds.first?.id
+    temperamentLbl.text = kittyDetails?.breeds.first?.temperament
+  
+    let url = URL(string: imageURL)
+    imgView.downloaded(from: url!)
     
     temperamentLbl.adjustsFontSizeToFitWidth = true
     temperamentLbl.sizeToFit()
-//    temperamentLbl.minimumScaleFactor = 1
     temperamentLbl.numberOfLines = 0
-//    temperamentLbl.lineBreakMode = .byWordWrapping
-      
-    }
-    
-
-  
-
+  }
 }
+extension UIImageView {
+    func downloaded(from url: URL, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+        contentMode = mode
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+        }.resume()
+    }
+    func downloaded(from link: String, contentMode mode: UIView.ContentMode = .scaleAspectFit) {  // for swift 4.2 syntax just use ===> mode: UIView.ContentMode
+        guard let url = URL(string: link) else { return }
+        downloaded(from: url, contentMode: mode)
+    }
+}
+
