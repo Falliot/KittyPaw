d2ef00c92215aaaad0e67ee782ee577cd2cfe18e
Anton <pustovidko.anton@icloud.com>
2020-05-09 14:56:09 +0300

    modify kittyData structure

--- Test Task/Test Task/KittyData.swift
+++ Test Task/Test Task/KittyData.swift
@@ -8,10 +8,64 @@
 
 import Foundation
 struct KittyData : Codable {
+  
+  let id: String
+  //let name: String
+  //let temperament: String
+  //let origin: String
+  //let imageData: ImageParams
+  
+  
+//  init(id: String, name: String, temperament: String, origin: String)// imageData: ImageParams)
+//  {
+//    self.id = id
+//    //    self.name = name
+//    //    self.temperament = temperament
+//    //    self.origin = origin
+//    //    self.imageData = imageData
+//  }
+  
+}
 
-let id: String
-let name: String
-let temperament: String
-let origin: String
+struct BreedImg: Codable {
+  var breeds = [Breeds]()
+  //  var id: String
+  var url: String
+  var width: Int
+  var height: Int
+  
+  
+//  init(breeds: [Breeds], url: String, width: Int, height: Int) {
+//    self.breeds = breeds
+//    self.url = url
+//    self.width = width
+//    self.height = height
+//  }
+  
+}
 
+struct Breeds: Codable {
+  let id: String
+  let name: String
+  let temperament: String
+  let origin: String
+  let wikipediaURL: String
+  
+  enum CodingKeys: String, CodingKey {
+    case id
+    case name
+    case temperament
+    case origin
+    case wikipediaURL = "wikipedia_url"
+  }
+  
+//  
+//  init(id: String, name: String, temperament: String, origin: String, wiki: String)
+//  {
+//    self.id = id
+//    self.name = name
+//    self.temperament = temperament
+//    self.origin = origin
+//    self.wikipediaURL = wiki
+//  }
 }
