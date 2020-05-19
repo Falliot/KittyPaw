385566552f0278b8a61a7a20ac222b43b1a64864
Anton <pustovidko.anton@icloud.com>
2020-05-19 14:59:15 +0300

    add activity indicator

--- Test Task/Test Task/Utility.swift
+++ Test Task/Test Task/Utility.swift
@@ -5,7 +5,6 @@
 //  Created by Anton on 5/7/20.
 //  Copyright © 2020 falli_ot. All rights reserved.
 //
-
 import UIKit
 
 let imageCache = NSCache<NSString, UIImage>()
@@ -14,39 +13,60 @@ class CustomImageView: UIImageView {
   
   var imageUrlString: String?
   
-  func downloadImage(urlString: String, placeholder: UIImage? = nil) {
+  let activityIndicator = UIActivityIndicatorView()
+  
+  func downloadImage(urlString: String) {
+      
+    // setup activityIndicator...
+    activityIndicator.style = .large
+    activityIndicator.color = .darkGray
+
+    addSubview(activityIndicator)
+    activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+    activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+    activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+
+    
     
     imageUrlString = urlString
     
     guard let url = URL(string: urlString) else { return }
     
-    image = placeholder
+    image = nil
+    activityIndicator.startAnimating()
     
     if let imageFromCache = imageCache.object(forKey: urlString as NSString) {
       self.image = imageFromCache
+      activityIndicator.stopAnimating()
       return
     }
     
     URLSession.shared.dataTask(with: url, completionHandler: { (data, respones, error) in
       
       if error != nil {
-        print(error ?? "")
+        print("Error occured during downloading: \(String(describing: error))")
+        DispatchQueue.main.async {
+         self.activityIndicator.stopAnimating()
+        }
         return
       }
       
       DispatchQueue.main.async {
-        guard let imageToCache = UIImage(data: data!) else { return }
+        
+        if let imageToCache = UIImage(data: data!) {
         
         print("Success: \(urlString)")
         
         if self.imageUrlString == urlString {
+          
           self.image = imageToCache
         }
+          imageCache.setObject(imageToCache, forKey: urlString as NSString)
+        }
         
-        imageCache.setObject(imageToCache, forKey: urlString as NSString)
+        self.activityIndicator.stopAnimating()
       }
       
     }).resume()
-  }
-  
+  }  
 }
